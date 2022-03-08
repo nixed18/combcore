8748dd352ad625e68cfcba098bd523f5e7301e20
nixed18@protonmail.com <denxiisme@gmail.com>
2022-03-08 04:37:25 -0500

    commitsbyblock

--- ghettorpc.go
+++ ghettorpc.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"libcomb"
 	"log"
 	"net"
 	"net/http"
+	"strconv"
 	"time"
-)
 
+	"github.com/gorilla/mux"
+)
 
 // Exists temporarily to serve scanner needs
 func ghetto_rpc() {
@@ -22,7 +23,7 @@ func ghetto_rpc() {
 	s0 := r.PathPrefix("/").Subrouter()
 	s0.HandleFunc("/push_block/{blockjson}", push_block)
 	s0.HandleFunc("/get_commit_count", get_commit_count)
-	s0.HandleFunc("/get_commits", get_commits)
+	s0.HandleFunc("get_block_commits/{block}", get_block_commits)
 	s0.HandleFunc("get_block_by_height/{height}", get_block_by_height)
 	s0.HandleFunc("get_block_by_hash/{hash}", get_block_by_hash)
 	s0.HandleFunc("/get_coinbase", get_coinbase)
@@ -45,8 +46,14 @@ func push_block(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func get_commits(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, fmt.Sprint(libcomb.GetCommits()))
+func get_block_commits(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	h, err := strconv.Atoi(vars["block"])
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprint(libcomb.GetBlockCommits(uint64(h))))
 }
 
 func get_commit_count(w http.ResponseWriter, r *http.Request) {
